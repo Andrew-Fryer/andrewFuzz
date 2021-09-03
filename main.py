eff3b93b0287888e8632bfa13a70549991d5df3b
Andrew Fryer <andrew.fryer@queensu.ca>
2021-09-03 10:32:10 -0400

    use composition

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ class ParsingProgress:
         return self.data_model, self.stream
 
 class Parser:
+    # -parser     -> it can parse a binary stream into a list of ParsingProgress objects (which contain new instances of this class)
     def parse(self, stream):
         pass
 
@@ -43,21 +44,26 @@ class AST:
         pass
 
 class Fuzzer:
+    # -fuzzer     -> it can produce mutated versions of itself (new instances of this class)
     def fuzz(self):
         pass
 
 class Serializer:
+    # -serializer -> it can deparse its state by performing an inorder tree traversal
     def serialize(self):
         pass
 
-class DataModel(Parser, Fuzzer, Serializer): # wait a minute, why don't I use composition here?
+class DataModel:
     # borrowing the name "DataModel" from Peach Fuzzer
+    def __init__(self, parser, fuzzer, serializer):
+        self._parser = parser
+        self._fuzzer = fuzzer
+        self._serializer = serializer
+    def clone(self, parser=self._parser, fuzzer=self._fuzzer, serializer=self._serializer):
+        return self.__class__(parser, fuzzer, serializer)
     # An instance of this class is a:
     # -grammar    -> writing a grammar is done by calling the constructor
-    # -parser     -> it can parse a binary stream into a list of ParsingProgress objects (which contain new instances of this class)
     # -AST        -> it stores 
-    # -fuzzer     -> it can produce mutated versions of itself (new instances of this class)
-    # -serializer -> it can deparse its state by performing an inorder tree traversal
     pass
 
 class NonTerminal(DataModel):
