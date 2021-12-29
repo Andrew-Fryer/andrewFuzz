44a398b1c9ec9a8338095311430ce96abc5b3d77
Andrew Fryer <andrew.fryer@queensu.ca>
2021-12-29 00:01:45 -0500

    Fuzz class to be used in future maybe

--- src/FuzzingProgress.py
+++ src/FuzzingProgress.py
@@ -1,7 +1,16 @@
-class ParsingProgress:
+from enum import Enum
+
+class Fuzz:
     # this class is a named tuple which stores the progress of a fuzz
-    def __init__(self, data_model, stream):
+    def __init__(self, data_model, fuzziness):
         self.data_model = data_model
-        self.stream = stream
+        self.fuzziness = fuzziness
     def get_tuple(self):
-        return self.data_model, self.stream
+        return self.data_model, self.fuzziness
+
+class Fuzziness(Enum):
+    correct = 1
+    semantic = 2
+    syntactic = 3
+    both = 4
+    unknown = 5
