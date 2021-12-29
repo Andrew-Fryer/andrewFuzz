54ae33f4848831c53d7b09033403501d437147e7
Andrew Fryer <andrew.fryer@queensu.ca>
2021-12-28 22:52:06 -0500

    remove Shell class

--- src/core_utils.py
+++ src/core_utils.py
@@ -1,18 +1,6 @@
 from src.BinaryStream import bitarray
 from src.ParsingProgress import ParsingProgress
 
-@DeprecationWarning
-class Shell:
-    # TODO: should this class create objects with a "set_value" method that actually turn an instance of this class into that class?
-    def __init__(self):
-        self.value = None # will be over-written by the caller
-    def parse(self, stream):
-        return self.value.parse(stream)
-    def fuzz(self):
-        return self.value.fuzz()
-    def serialize(self):
-        return self.value.serialize()
-
 class Literal:
     def __init__(self, literal_value, num_bits=None):
         if isinstance(literal_value, str):
