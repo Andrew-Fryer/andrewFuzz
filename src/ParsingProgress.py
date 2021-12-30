23083bf209fa47c42148c5b84b5dd90684915820
Andrew Fryer <andrew.fryer@queensu.ca>
2021-12-29 20:15:11 -0500

    Revert "try to monadify the ParsingProgress mechanism with the goal of abstracting complexity out of the parsing and fuzzing methods of NonTerminal classes"
    This reverts commit 4d42854d879248aaae59dbb29affc1dd8cc8e9cf.

--- src/ParsingProgress.py
+++ src/ParsingProgress.py
@@ -1,28 +1,7 @@
-from itertools import chain, product
-
-class Parse:
-    # this class is essentially the Haskell List Monad
-    def __init__(self, iterator):
-        # iterator is of (DataModel, BinaryStream) tuples
-        self.iterator = iterator
-    def _map(self, parse_function):
-        for data_model, stream in self.iterator:
-            iter = parse_function(stream)
-            yield (data_model, iter)
-    def _flatten(self, double_wrapped, join):
-        for data_model_1, iter in double_wrapped:
-            for data_model_2, remaining_stream in iter:
-                yield join(data_model_1, data_model_2), remaining_stream
-    def flat_map(self, parse_function, join):
-        # `join` is a function that takes 2 data models and 
-        return Parse(self.flatten(self.map(parse_function), join))
-    def flat_map_add(self, parse_function):
-        return self.flat_map(parse_function, chain)
-    def flat_map_mult(self, parse_function):
-        return self.flat_map(parse_function, product)
-
-class Fuzz:
-    def __init__(self, iterator):
-        # iterator is of (DataModel, Fuzziness) tuples
-        self.iterator = iterator
-    # def 
+class ParsingProgress:
+    # this class is a named tuple which stores the progress of a parse
+    def __init__(self, data_model, stream):
+        self.data_model = data_model
+        self.stream = stream
+    def get_tuple(self):
+        return self.data_model, self.stream
